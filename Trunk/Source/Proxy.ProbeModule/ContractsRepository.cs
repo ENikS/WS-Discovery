56476758e711b2e7b2bfbe725e47b39bbee54c2b
Eugene Sadovoi <ENikS@users.noreply.github.com>
2011-10-29 18:05:00 +0000

    Added stub code

--- Trunk/Source/Proxy.ProbeModule/ContractsRepository.cs
+++ Trunk/Source/Proxy.ProbeModule/ContractsRepository.cs
@@ -1,13 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.ServiceModel.Discovery;
+using System.Threading.Tasks;
 
 namespace Proxy.ProbeModule
 {
     [Export]
+    [PartCreationPolicy(CreationPolicy.Shared)]
     public class ContractsRepository
     {
+        //-----------------------------------------------------
+        //  Methods
+        //-----------------------------------------------------
+
+        #region Methods
+
+
+        #endregion
+
+        //-----------------------------------------------------
+        //  Task factories
+        //-----------------------------------------------------
+
+        #region Task Factories
+
+        /// <summary>
+        /// Creates Task which processes Online Announcement message.
+        /// </summary>
+        /// <param name="messageSequence"><see cref="DiscoveryMessageSequence"/> header info</param>
+        /// <param name="endpointDiscoveryMetadata"><see cref="EndpointDiscoveryMetadata"/> information</param>
+        /// <returns>Returns <see cref="Task"/> object encapsulating message handler</returns>
+        [Export(ContractName.OnlineAnnouncement, typeof(Func<DiscoveryMessageSequence, EndpointDiscoveryMetadata, Task>))]
+        public Task OnlineAnnouncementTaskFactory(DiscoveryMessageSequence messageSequence, EndpointDiscoveryMetadata endpointDiscoveryMetadata)
+        {
+            return Task.Factory.StartNew(() => Debug.WriteLine("AnnounceOnline task."));
+        }
+
+        /// <summary>
+        /// Creates Task which processes Offline Announcement message.
+        /// </summary>
+        /// <param name="messageSequence"><see cref="DiscoveryMessageSequence"/> header info</param>
+        /// <param name="endpointDiscoveryMetadata"><see cref="EndpointDiscoveryMetadata"/> information</param>
+        /// <returns>Returns <see cref="Task"/> object encapsulating message handler</returns>
+        [Export(ContractName.OfflineAnnouncement, typeof(Func<DiscoveryMessageSequence, EndpointDiscoveryMetadata, Task>))]
+        public Task OfflineAnnouncementTaskFactory(DiscoveryMessageSequence messageSequence, EndpointDiscoveryMetadata endpointDiscoveryMetadata)
+        {
+            return Task.Factory.StartNew(() => Debug.WriteLine("AnnounceOffline task."));
+        }
+
+        /// <summary>
+        /// Creates a Task which processes Probe request
+        /// </summary>
+        /// <param name="findRequestContext">Criteria for finding correct endpoints</param>
+        /// <returns>Returns <see cref="Task"/> object which encapsulates request handler</returns>
+        [Export(ContractName.Find, typeof(Func<FindRequestContext, Task>))]
+        public Task FindTaskFactory(FindRequestContext findRequestContext)
+        {
+            return Task.Factory.StartNew(() => Debug.WriteLine("FindTaskFactory"));
+        }
+
+        #endregion
     }
 }

83cc386eb6d3522af4f19bdcc67992818bc2c55e
ENikS_cp <SND\ENikS_cp>
2011-10-29 18:24:36 +0000

    Added Test priject

---
+++ Trunk/Tests/UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WS-Discovery Tests")]
+[assembly: AssemblyDescription("Tests for WS-Discovery project")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ENIKS, Inc.")]
+[assembly: AssemblyProduct("WS-Discovery Proxy")]
+[assembly: AssemblyCopyright("Copyright © ENIKS, Inc. 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("98cb65dd-060e-4647-b70f-59223c0055ee")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

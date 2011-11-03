a2d3e0fe8c9d2dfcc6f44391d143e5656b787c74
ENikS_cp <SND\ENikS_cp>
2011-11-03 20:16:05 +0000

    Added host windows service

---
+++ Trunk/Source/Proxy.Service.Host/ServiceHostInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+
+
+namespace System.ServiceModel.Discovery.ProxyService.Host
+{
+    [RunInstaller(true)]
+    public partial class ServiceHostInstaller : System.Configuration.Install.Installer
+    {
+        public ServiceHostInstaller()
+        {
+            InitializeComponent();
+        }
+    }
+}

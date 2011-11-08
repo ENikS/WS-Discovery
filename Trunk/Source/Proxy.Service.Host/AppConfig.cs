2d97fa16f67ce1abfe90d86f26afcbb60ed69c43
ENikS_cp <SND\ENikS_cp>
2011-11-08 23:03:31 +0000

    Added custom installers for firewall and app.config

---
+++ Trunk/Source/Proxy.Service.Host/AppConfig.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Configuration;
+using System.ServiceModel.Configuration;
+using System.Diagnostics;
+
+
+namespace Host.App.Config
+{
+    [RunInstaller(true)]
+    public partial class AppConfig : System.Configuration.Install.Installer
+    {
+        private const string _multicast = "Multicast";
+        
+        public AppConfig()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+
+            if (!stateSaver.Contains(_multicast))
+                stateSaver.Add(_multicast, (object)true);
+
+        }
+
+        public override void Uninstall(IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+        }
+
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+
+            Configuration config = ConfigurationManager.OpenExeConfiguration(this.GetType().Assembly.Location);
+
+            ConfigurationSectionGroup serviceModelGroup = config.SectionGroups["system.serviceModel"];
+            ConfigurationSection serviceSection = serviceModelGroup.Sections["services"];
+            ServiceElement proxyService = (serviceSection as ServicesSection).Services["System.ServiceModel.Discovery.ProxyService"];
+
+            // Add Endpoints
+            proxyService.Endpoints.Add(new ServiceEndpointElement() { Name = "UdpMulticastEndpoint", IsSystemEndpoint = false, Kind = "udpAnnouncementEndpoint" });
+
+            Debug.WriteLine("Section Groups : ");
+            foreach (var service in (serviceSection as ServicesSection).Services)
+            {
+                Debug.WriteLine(service);
+            }
+
+            config.Save();
+        }
+
+        public override void Rollback(IDictionary savedState)
+        {
+            base.Rollback(savedState);
+        }
+    }
+}
---
+++ Trunk/Source/Proxy.Service.Host/FairwallInstaller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+
+
+namespace System.ServiceModel.Discovery.ProxyService.Host
+{
+    [RunInstaller(true)]
+    public partial class FairwallInstaller : System.Configuration.Install.Installer
+    {
+        public FairwallInstaller()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+        }
+
+        public override void Uninstall(IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+        }
+    }
+}
